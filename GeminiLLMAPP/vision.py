ccb6c55f831bc818d5c7aa29f5f147efadbc29c0
PraneethChandG <praneethchand99@gmail.com>
2025-04-20 17:13:29 +0530

    image analyser

--- GeminiLLMAPP/vision.py
+++ GeminiLLMAPP/vision.py
@@ -6,11 +6,12 @@ load_dotenv() ## loading environment variables
 import os
 import streamlit as st
 import google.generativeai as genai
+from PIL import Image
 
 genai.configure(api_key= os.getenv("GOOGLE_API_KEY")) ## configure google api 
 
 ## function to load gemini pro model and get responses
-model = genai.GenerativeModel("gemini-pro-vision")
+model = genai.GenerativeModel("gemini-2.0-flash")
 
 def get_gemini_response(input,img):
     if not input:
@@ -30,4 +31,14 @@ uploaded_file = st.file_uploader("choose an image.. ", type = ["jpg", "jpeg", "p
 img = ""
 
 if uploaded_file is not None:
-    pass
+    img = Image.open(uploaded_file)
+    st.image(img, caption='Uploaded Image', use_container_width=True)
+
+submit = st.button("tell me about the image")
+
+## if submit is clicked
+if submit:
+    response = get_gemini_response(input, img)
+
+    st.subheader("The Response is")
+    st.write(response)
